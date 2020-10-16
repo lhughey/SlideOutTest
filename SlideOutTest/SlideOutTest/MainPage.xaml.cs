94568e75a5719b4efb7c45a6039fb1dc038249d9
Lyndon Hughey <lhughey@gmail.com>
2020-10-16 08:51:46 -0500

    Add project files.

---
+++ SlideOutTest/SlideOutTest/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SlideOutTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //BindingContext = new MainPageViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //((MainPageViewModel)BindingContext).AlertCommand.Execute(0);
+        }
+    }
+}
---
+++ SlideOutTest/SlideOutTest/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Xamarin.Forms;
+
+namespace SlideOutTest
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        public ICommand FavoritePatientCommand { get; }
+        public ICommand AlertCommand { get; }
+
+        [Reactive] public bool IsBusy { get; set; }
+
+        public MainPageViewModel()
+        {
+            FavoritePatientCommand =
+                ReactiveCommand.CreateFromTask(execute: async () => await FavoriteAction()); //.OnExecuting(b => IsBusy = b);
+
+            AlertCommand = new Command(async () =>
+            {
+                await UserDialogs.Instance.AlertAsync("Alert Command Pressed", "Alert");
+            });
+        }
+
+
+        public async Task FavoriteAction()
+        {
+            await UserDialogs.Instance.AlertAsync("Favorited Command", "Alert");
+            //Load the 
+        }
+    }
+}
